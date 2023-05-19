fdad014e64a3b474babfde8eb4fe23f177f127a4
Alexsandro de Lima Silva <alexsandrolima.dev@gmail.com>
2023-05-19 15:11:42 -0300

    Initial Project

---
+++ src/main/java/com/itau/test/ItauNttTestApplication.java
@@ -0,0 +1,13 @@
+package com.itau.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauNttTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauNttTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/test/ItauNttTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauNttTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
